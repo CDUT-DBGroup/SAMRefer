fbecb4972d8c3cef49703755bb86fdf97568193d
munan <1491761293@qq.com>
2025-09-04 16:54:11 +0800

    修复该分支的问题

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,5 +5,5 @@ Ctrl + A，然后松手，按 D
 
 nohup ./scripts/train_1.sh > train_0628.log 2>&1 &
 
-nohup ./scripts/run_deepspeed.sh > train_0717.log 2>&1 &
+nohup ./scripts/run_deepspeed.sh > train_0904-1-refcoco.log 2>&1 &
 
